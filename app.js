57e704cc10744f1d48057a78fe0fe8ebeef23b89
Jose Pieters <jose.pieters@Hotmail.com>
2020-03-30 10:40:58 -0400

    Updated files work in progress...

---
+++ app.js
@@ -0,0 +1,9 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const render = require("./lib/htmlRenderer")
+const fs = require("fs");
+
+
